a768dfcc77887c3d0dbd5955548308423f61a404
Mohammad <mohammadkh12221@gmail.com>
2024-10-12 00:51:57 -0700

    music player section

---
+++ src/context/PlayerContext.jsx
@@ -0,0 +1,72 @@
+import { createContext, useEffect, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
+
+export const PlayerContext = createContext();
+
+const PlayerContextProvider = (props) => {
+  const audioRef = useRef();
+  const seekBg = useRef();
+  const seekBar = useRef();
+
+  const [track, setTrack] = useState(songsData[1]);
+  const [playStatus, setPalyStatus] = useState(false);
+  const [time, setTime] = useState({
+    currentTime: {
+      second: 0,
+      minute: 0,
+    },
+    totalTime: {
+      second: 0,
+      minute: 0,
+    },
+  });
+
+  const play = () => {
+    audioRef.current.play();
+    setPalyStatus(true);
+  };
+
+  const pause = () => {
+    audioRef.current.pause();
+    setPalyStatus(false);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      audioRef.current.ontimeupdate = () => {
+        setTime({
+          currentTime: {
+            second: Math.floor(audioRef.current.currentTime % 60),
+            minute: Math.floor(audioRef.current.currentTime / 60),
+          },
+          totalTime: {
+            second: Math.floor(audioRef.current.duration % 60),
+            minute: Math.floor(audioRef.current.duration / 60),
+          },
+        });
+      };
+    }, 1000);
+  }, [audioRef]);
+
+  const contextValue = {
+    audioRef,
+    seekBar,
+    seekBg,
+    track,
+    setTrack,
+    playStatus,
+    setPalyStatus,
+    time,
+    setTime,
+    play,
+    pause,
+  };
+
+  return (
+    <PlayerContext.Provider value={contextValue}>
+      {props.children}
+    </PlayerContext.Provider>
+  );
+};
+
+export default PlayerContextProvider;
