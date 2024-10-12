53115fad1c31c33726642e4f31f430efd5c2e82f
Mohammad <mohammadkh12221@gmail.com>
2024-10-11 23:19:26 -0700

    created Alubm List And Song List

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
+import Display from "./components/Display";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar />
+        <Display />
       </div>
       <Player />
     </div>
